c1e1ce72680c4a31007a78c3ef0a3081167fa925
Владимир Леонтьев <dllreg.exe@gmail.com>
2023-12-16 14:17:49 +0800

    Initial commit

---
+++ market/utils.py
@@ -0,0 +1,25 @@
+import os
+
+
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
+
+
+def getenv(key, default=None):
+    """Get an environment variable, return None if it doesn't exist or blank.
+    The optional second argument can specify an alternate default.
+    key, default and the result are str."""
+    value = os.environ.get(key, default)
+    return value if value != '' else default
